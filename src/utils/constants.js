acbb628345fa35ca16cd4d16f0bd0aaa8de52d13
sravaninaalam <sravaninaalam2000@gmail.com>
2023-08-22 11:55:42 +0530

    shorts api added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
 
 const Google_Api_Key="AIzaSyCYWlnZmZw4Smk5Qw8quLIhdaOjRultq-s"
+
 export const Youtube_Video_Api=
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+
   Google_Api_Key
 
-  export const Youtube_Suggestions="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+  export const Youtube_Suggestions= "https://corsproxy.io/?https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  // "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 
   export const Search_By_Keyword='https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&key='+Google_Api_Key+"&q="
@@ -14,3 +16,8 @@ export const Youtube_Video_Api=
 
 
   export const Live_Chat_Count=20
+
+
+  export const SHORTS_API="https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&type=video&videoDuration=short&key=" +
+  Google_Api_Key +
+  "&q=trendingshorts";
